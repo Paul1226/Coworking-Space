ef99527f7f93f346c9054c8554396f1ffa7cd0b5
Paul1226 <paulguachichullca023@gmail.com>
2024-04-03 19:47:49 -0500

    Landing creado solamente para un dispositivo mobile por el momento

---
+++ contentPage.js
@@ -0,0 +1,22 @@
+export const nav = [
+    {
+        name: 'Home',
+        link: '/'
+    },
+    {
+        name: 'About',
+        link: '/About'
+    },
+    {
+        name: 'Featured',
+        link: '/Featured'
+    },
+    {
+        name: 'Blog',
+        link: '/Blog'
+    },
+    {
+        name: 'More',
+        link: '/More'
+    }
+]
---
+++ tailwind.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors:{
+				primary: '#9B9A78',
+				secondary: '#313131',
+				tertiary: '#909090',
+				quaternary: '#7290A0',
+			},
+			fontFamily: {
+				poppins: ['Poppins', 'sans-serif'],
+				playfair: ['Playfair Display Variable', 'serif'],
+			},
+			gridTemplateColumns:{
+				'2mobile': 'max-content max-content',
+				'3p': 'repeat(auto-fill, minmax(250px,1fr))',
+			}
+		},
+	},
+	plugins: [],
+}
